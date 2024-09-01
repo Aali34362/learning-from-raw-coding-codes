4336317027659e45d12ee74535ddd8fda3433f73
Aamir Ali <aa882755@gmail.com>
2024-09-01 09:27:01 +0530

    semaphore with Http client

--- Semaphore/Semaphore/HttpClientProgram.cs
+++ Semaphore/Semaphore/HttpClientProgram.cs
@@ -1,14 +1,16 @@
 ﻿using Dumpify;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SemaphoreCode;
 
 public static class HttpClientProgram
 {
-    static HttpClient _client = new HttpClient();
+    static HttpClient _client = new HttpClient() { Timeout = TimeSpan.FromSeconds(5) };
     static SemaphoreSlim _gate = new(1);
+    //it protects a certain process asynchronously.
     public static void HttpClientProgramMain()
     {
-        CreateCalls();
+        Task.WaitAll(CreateCalls().ToArray());
     }
 
     public static IEnumerable<Task> CreateCalls()
@@ -21,7 +23,16 @@ public static class HttpClientProgram
 
     public static async Task CallGoogle()
     {
-        var response = await _client.GetAsync("https://google.com");
-        response.StatusCode.Dump();
+        try
+        {
+            await _gate.WaitAsync();
+            var response = await _client.GetAsync("https://google.com");
+            _gate.Release();
+            response.StatusCode.Dump();
+        }
+        catch(Exception e)
+        {
+            e.Message.Dump();
+        }
     }
 }
