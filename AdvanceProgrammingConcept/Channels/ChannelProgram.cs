c9e84071ec53023439eaf3bb628a1a61b9537b59
Aamir Ali <aa882755@gmail.com>
2024-09-05 10:44:28 +0530

    Channels

---
+++ AdvanceProgrammingConcept/Channels/ChannelProgram.cs
@@ -0,0 +1,38 @@
+﻿using Dumpify;
+
+namespace Channels;
+
+public static class ChannelProgram
+{
+    public static void ChannelProgramMain()
+    {
+        ProcessSendNotificationUsingFireAndForget();
+        ProcessSendNotification();
+    }
+
+    public static void ProcessSendNotificationUsingFireAndForget()
+    {
+        //using Fire and Forget Task
+        for (int i = 0; i < 10; i++)
+        {
+            i.Dump();
+            Task.Run(() => SendNotification(i));
+        }
+    }
+
+    public static void ProcessSendNotification()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            i.Dump();
+            SendNotification(i);
+        }
+    }
+
+    public static void SendNotification(int i)
+    {
+        //blocking operation
+        Task.Delay(1000).GetAwaiter().GetResult();
+        $"Complete : {i}".Dump();
+    }
+}
