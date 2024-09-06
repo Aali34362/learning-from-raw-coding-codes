50513aaed10edd95a54ee7f114a292a4ca1c8d6c
Aamir Ali <aa882755@gmail.com>
2024-09-06 09:52:07 +0530

    Using Reflection to Create Dynamic MVC

--- AdvanceProgrammingConcept/ModelViewController/mvc_the_solution.cs
+++ AdvanceProgrammingConcept/ModelViewController/mvc_the_solution.cs
@@ -1,4 +1,5 @@
 ﻿using Dumpify;
+using System.Reflection;
 
 namespace ModelViewController;
 
@@ -6,39 +7,68 @@ public class mvc_the_solution
 {
     public void MVCSolutionMain()
     {
-        var uri = new Uri("http://localhost/home/index");
+        var uri = new Uri("http://localhost/home/index?msg=HelloWorld");
         var container = new MvcContainer();
-        container.Resolve(uri);
+        var result = container.Resolve(uri);
+        result.Dump();
     }
 }
 
 public class MvcContainer
 {
+    //Reflection solve the problem by making things Dynamic
     List<Type> controllerTypes = [];
     public MvcContainer()
     {
         //Extracting Controllers which extends Controller
         var controllerType = typeof(Controller);
-        var controllerTypes = controllerType.Assembly.GetTypes()
+        controllerTypes = controllerType.Assembly.GetTypes()
             .Where(type => !type.IsAbstract && controllerType.IsAssignableFrom(type)).ToList();
-        controllerTypes.Dump();
-        controllerTypes.AddRange(controllerTypes);
+        controllerTypes.Dump("Fetching all existing Controllers in Assembly");
+        //controllerTypes.AddRange(controllerTypes);
     }
     public object Resolve(Uri uri)
     {
         var controller = getController(uri);
-        return null;
+        controller.Dump("Fetch Controller From URI");
+
+        var action = getAction(controller, uri);
+        return action.Invoke(controller, null)!;
     }
-    public Controller getController(Uri uri)
+    private Controller getController(Uri uri)
     {
-        return null;
+        var controllerType = controllerTypes
+                            .FirstOrDefault(x => uri.AbsolutePath
+                                .StartsWith($"/{x.Name.Replace("Controller","")}", 
+                                    StringComparison.InvariantCultureIgnoreCase));
+        return (Controller)Activator.CreateInstance(controllerType!, null)!;
     }
+    private MethodInfo getAction(Controller controller, Uri uri)
+    {
+        //controller.GetType().DeclaringMethod.Dump();
+        var action = uri.AbsolutePath.Split('/').Last().Dump("Fetching Action Name from Uri Absolute");
+        controller
+            .GetType()
+            .GetMembers()
+            .FirstOrDefault(x => x.Name.Equals(action, StringComparison.InvariantCultureIgnoreCase))
+            .Dump("Fetching Action Method from Controller Class comparing with Uri Action");
+
+        return controller
+            .GetType()
+            .GetMethods()
+            .FirstOrDefault(x => x.Name.Equals(action, StringComparison.InvariantCultureIgnoreCase))!;
+    }
+    //
 }
 
 public abstract class Controller { }
 public class HomeController : Controller 
 {
     public string Index()
+    {
+        return "Home Controller";
+    }
+    public string Test()
     {
         return "Home Controller";
     }
