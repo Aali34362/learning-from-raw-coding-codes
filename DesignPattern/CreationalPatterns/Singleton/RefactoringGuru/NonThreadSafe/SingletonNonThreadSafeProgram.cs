459c0b08723ddaeaee887540342ede941db23641
Amir Abdul Wahid <aa882755@gmail.com>
2024-10-09 07:14:52 +0530

    Singleton

---
+++ DesignPattern/CreationalPatterns/Singleton/RefactoringGuru/NonThreadSafe/SingletonNonThreadSafeProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace Singleton.RefactoringGuru.NonThreadSafe;
+
+public static class SingletonNonThreadSafeProgram
+{
+    public static void SingletonNonThreadSafeProgramMain()
+    {
+        // The client code.
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+
+        if (s1 == s2)
+        {
+            Console.WriteLine("Singleton works, both variables contain the same instance.");
+        }
+        else
+        {
+            Console.WriteLine("Singleton failed, variables contain different instances.");
+        }
+    }
+}
